501f2acdb944d589565f42e9ade405990e197420
quanting56 <qwerdgt@gmail.com>
2025-06-09 16:46:19 +0800

    網頁框架v1.01

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/ubike-map/'  // 這裡請填你的 GitHub repo 名稱
+    : '/',
   transpileDependencies: true,
+  pages: {
+    index: {
+      entry: 'src/main.js', // 或main.ts，視專案而定
+      title: 'Ubike微笑單車地圖',
+    }
+  }
 });
