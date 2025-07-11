7e05ab538de4904cbda69ba3f04cf2fb6bbb480c
quanting56 <qwerdgt@gmail.com>
2025-07-11 12:07:59 +0800

    Updates

--- js/about.5cd06e23.js
+++ js/about.5cd06e23.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkproject2"]=self["webpackChunkproject2"]||[]).push([[594],{603:function(n,t,u){u.r(t),u.d(t,{default:function(){return i}});var e=u(6768);const r={class:"about"};function a(n,t){return(0,e.uX)(),(0,e.CE)("div",r,t[0]||(t[0]=[(0,e.Lk)("h1",null,"This is an about page",-1)]))}var c=u(1241);const s={},o=(0,c.A)(s,[["render",a]]);var i=o}}]);
-//# sourceMappingURL=about.eae4f6a9.js.map
+//# sourceMappingURL=about.5cd06e23.js.map
