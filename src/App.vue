d25170d333cea6f346af805ede21b44ebd07a707
quanting56 <qwerdgt@gmail.com>
2025-07-16 12:38:13 +0800

    fix: 修改README.md文件

--- src/App.vue
+++ src/App.vue
@@ -184,7 +184,10 @@ nav {
 /* Loading 覆蓋層 */
 .loading-container {
   position: absolute;
-  top: 0; left: 0; right: 0; bottom: 0;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
   background: rgba(0, 0, 0, 0.5);
   display: flex;
   flex-direction: column;
